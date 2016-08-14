6a6212e7b010cdf5e5a58b49d4079027ebf6e4e8
Park Ju Hyung <qkrwngud825@gmail.com>
2016-08-14 23:45:06 +0900

    build_master: underp
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 if [ ! "${1}" = "skip" ] ; then
 	./build_clean.sh
-	./build_kernel.sh CC='$(CROSS_COMPILE)gcc' "$@" || exit 1
-	./build_recovery.sh CC='$(CROSS_COMPILE)gcc' "$@" || exit 1
+	./build_kernel.sh "$@" || exit 1
+	./build_recovery.sh "$@" || exit 1
 fi
 
 if [ -e boot.img ] ; then
 	rm arter97-kernel-"$(cat version)".zip 2>/dev/null
 	cp boot.img kernelzip/boot.img
-	tail -n $(($(cat ramdisk/default.prop | wc -l) - $(grep -n "START OVERRIDE" ramdisk/default.prop | cut -d : -f 1) + 1)) ramdisk/default.prop > kernelzip/kernel.prop
 	cd kernelzip/
 	7z a -mx9 arter97-kernel-"$(cat ../version)"-tmp.zip *
 	zipalign -v 4 arter97-kernel-"$(cat ../version)"-tmp.zip ../arter97-kernel-"$(cat ../version)"-"$(date +%F | sed s@-@@g)".zip
 	rm arter97-kernel-"$(cat ../version)"-tmp.zip
 	cd ..
 	ls -al arter97-kernel-"$(cat version)"-"$(date +%F | sed s@-@@g)".zip
-	rm kernelzip/boot.img kernelzip/kernel.prop
+	rm kernelzip/boot.img
 fi
 
 if [ -e recovery.img ] ; then
