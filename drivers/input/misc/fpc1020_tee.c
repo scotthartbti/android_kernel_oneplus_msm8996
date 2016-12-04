1ff3eaef171ff6065789b86505be87b04de22a28
Sultanxda <sultanxda@gmail.com>
2016-12-03 18:36:14 -0800

    input: synaptics: s3320: Disable gestures when proximity is covered
    Add a dirty hook to the fpc driver to disable off-screen gestures when the
    proximity sensor is covered in order to prevent unwanted behavior, such as
    off-screen gestures going off when the phone is in a pocket or during voice
    calls when the phone is touching the side of someone's face.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- drivers/input/misc/fpc1020_tee.c
+++ drivers/input/misc/fpc1020_tee.c
@@ -300,6 +300,7 @@ static DEVICE_ATTR(irq, S_IRUSR | S_IWUSR, irq_get, irq_ack);
 extern void int_touch(void);
 extern struct completion key_cm;
 extern bool virtual_key_enable;
+extern void s3320_disable_gestures(bool disable);
 
 bool key_home_pressed = false;
 EXPORT_SYMBOL(key_home_pressed);
@@ -401,6 +402,7 @@ static ssize_t proximity_state_set(struct device *dev,
 		return -EINVAL;
 
 	fpc1020->proximity_state = !!val;
+	s3320_disable_gestures(fpc1020->proximity_state);
 
 	if (!fpc1020->screen_state)
 		set_fpc_irq(fpc1020, !fpc1020->proximity_state);
