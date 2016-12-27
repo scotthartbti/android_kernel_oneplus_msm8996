3dc6a52ae319be6b6dab68ea9e58a270ccd3157b
scotthartbti <scott.hart.bti@gmail.com>
2016-12-26 23:32:52 -0500

    Revert "qcacld: merge LA.UM.5.5.r1-02200-8x96.0"
    This reverts commit 7ad8cfe0dc56cc2abfde18e7de45da3626603cec.

--- drivers/staging/qcacld-2.0/CORE/SME/src/csr/csrApiRoam.c
+++ drivers/staging/qcacld-2.0/CORE/SME/src/csr/csrApiRoam.c
@@ -5875,11 +5875,11 @@ static tANI_BOOLEAN csrRoamProcessResults( tpAniSirGlobal pMac, tSmeCmd *pComman
             */
             //csrRoamStateChange( pMac, eCSR_ROAMING_STATE_JOINED );
 
-            /* Reset full_power_till_set_key as it might have been set
+            /* Reset remainInPowerActiveTillDHCP as it might have been set
              * by last failed secured connection.
              * It should be set only for secured connection.
              */
-            pMac->pmc.full_power_till_set_key = false;
+            pMac->pmc.remainInPowerActiveTillDHCP = FALSE;
             if( CSR_IS_INFRASTRUCTURE( pProfile ) )
             {
                 pSession->connectState = eCSR_ASSOC_STATE_TYPE_INFRA_ASSOCIATED;
@@ -6229,14 +6229,15 @@ static tANI_BOOLEAN csrRoamProcessResults( tpAniSirGlobal pMac, tSmeCmd *pComman
             //enough to let security and DHCP handshake succeed before entry into BMPS
             if (!pMac->psOffloadEnabled && pmcShouldBmpsTimerRun(pMac))
             {
-                /* Set full_power_till_set_key to make sure we wait for
+                /* Set remainInPowerActiveTillDHCP to make sure we wait for
                  * until keys are set before going into BMPS.
                  */
                 if(eANI_BOOLEAN_TRUE == roamInfo.fAuthRequired)
                 {
-                     pMac->pmc.full_power_till_set_key = true;
-                     smsLog(pMac, LOG1,
-                       FL("Set full_power_till_set_key to make sure we wait until keys are set before going to BMPS"));
+                     pMac->pmc.remainInPowerActiveTillDHCP = TRUE;
+                     smsLog(pMac, LOG1, FL("Set remainInPowerActiveTillDHCP "
+                            "to make sure we wait until keys are set before"
+                            " going to BMPS"));
                 }
                 if (pmcStartTrafficTimer(pMac, BMPS_TRAFFIC_TIMER_ALLOW_SECURITY_DHCP)
                     != eHAL_STATUS_SUCCESS)
