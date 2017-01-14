a92752f4846305d9ce8a93c0ac791671b3029ffc
scotthartbti <scott.hart.bti@gmail.com>
2017-01-13 22:35:31 -0500

    Revert "msm: camera: cpp: Add validation for v4l2 ioctl arguments"
    This reverts commit e5f7609f710ba33b22bc1d5bd16b977061eb8358.

--- drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
+++ drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
@@ -2688,14 +2688,14 @@ static int msm_cpp_validate_input(unsigned int cmd, void *arg,
 		break;
 	default: {
 		if (ioctl_ptr == NULL) {
-			pr_err("Wrong ioctl_ptr for cmd %u\n", cmd);
+			pr_err("Wrong ioctl_ptr %pK\n", ioctl_ptr);
 			return -EINVAL;
 		}
 
 		*ioctl_ptr = arg;
 		if ((*ioctl_ptr == NULL) ||
 			((*ioctl_ptr)->ioctl_ptr == NULL)) {
-			pr_err("Error invalid ioctl argument cmd %u", cmd);
+			pr_err("Wrong arg %pK\n", arg);
 			return -EINVAL;
 		}
 		break;
